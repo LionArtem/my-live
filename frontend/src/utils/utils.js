4239883408d137fdd3907dab8c9962bd33cb7f91
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-08-13 16:18:42 +0300

    переписать функцию getTimeLocal используя методы объекта date

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,8 +1,20 @@
 const getTimeLocal = (date) => {
-  const timeLocal = new Date(date).toString();
-  const arrTimeLocal = timeLocal.split(' ');
-  const [month, number, year, time] = arrTimeLocal.slice(1, 5);
-  const strTimeLocal = `${month} ${number} ${year} ${time}`;
+  //const timeLocal = new Date(date).toString();
+  //const arrTimeLocal = timeLocal.split(' ');
+  //const [month, number, year, time] = arrTimeLocal.slice(1, 5);
+  // const strTimeLocal = `${month} ${number} ${year} ${time}`;
+
+  const timeLocal = new Date(date);
+  const month = timeLocal.getMonth() + 1;
+  const number = timeLocal.getDate();
+  const year = timeLocal.getFullYear();
+  const hours = timeLocal.getHours();
+  const minutes = timeLocal.getMinutes();
+  const seconds = timeLocal.getSeconds();
+  const strTimeLocal = `${month}.${
+    number < 10 ? '0' + number : number
+  }.${year} ${hours}:${minutes}:${seconds}`;
+
   return strTimeLocal;
 };
 
