2f2dd23731a6f9b1b931b4af42486618769f76e0
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-12-02 16:51:21 +0300

    добавить информацию на страницу user

--- frontend/src/components/Pages/PageUser/PageUser.jsx
+++ frontend/src/components/Pages/PageUser/PageUser.jsx
@@ -1,18 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Style from './PageUser.module.scss';
 import { usersApi } from '../../../utils/UserApi';
+import ErrServer from '../../ErrServer/ErrServer';
 
 export default function PageUser() {
+  const [user, isUser] = useState();
+  const [err, isErr] = useState(false);
+  const [errText, isErrText] = useState('');
+
   useEffect(() => {
     usersApi
       .getUserId(localStorage.getItem('userId'))
       .then((user) => {
-        console.log(user);
+        isUser(user);
       })
       .catch((err) => {
-        console.log(err);
+        isErr(true);
+        isErrText(err.message);
       });
   }, []);
 
-  return <div>PageUser</div>;
+  if (err) {
+    return <ErrServer textErr={errText} />;
+  }
+
+  if (!user) {
+    return;
+  }
+
+  return (
+    <div className={Style.PageUser}>
+      <img
+        className={Style.foto}
+        src={
+          user.avatar
+            ? `http://localhost:3001/${user.avatar}`
+            : 'https://www.murrayglass.com/wp-content/uploads/2020/10/avatar-scaled.jpeg'
+        }
+        alt="аватарка"
+      />
+      <p>{user.name}</p>
+      <p>{`(${user.gender}. ${user.age})`}</p>
+      <p>{user.town}</p>
+    </div>
+  );
 }
