b0c576068ce6eeea2f02fd41c63f2340bc893ebf
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-12-10 18:33:17 +0300

    добавить скелетон на страницу с юзером

--- frontend/src/components/Pages/PageUser/PageUser.jsx
+++ frontend/src/components/Pages/PageUser/PageUser.jsx
@@ -3,13 +3,16 @@ import Style from './PageUser.module.scss';
 import { usersApi } from '../../../utils/UserApi';
 import ErrServer from '../../ErrServer/ErrServer';
 import ButtonsNavigation from '../../Buttons/ButtonsNavigation/ButtonsNavigation';
+import PageUserSceleton from './PageUserSceleton';
 
 export default function PageUser() {
   const [user, isUser] = useState();
   const [err, isErr] = useState(false);
   const [errText, isErrText] = useState('');
+  const [showSceleton, isShowSceleton] = useState(false);
 
   useEffect(() => {
+    isShowSceleton(true);
     usersApi
       .getUserId(localStorage.getItem('userId'))
       .then((user) => {
@@ -18,16 +21,9 @@ export default function PageUser() {
       .catch((err) => {
         isErr(true);
         isErrText(err.message);
-      });
+      })
+      .finally(() => isShowSceleton(false));
   }, []);
-
-  if (err) {
-    return <ErrServer textErr={errText} />;
-  }
-
-  if (!user) {
-    return;
-  }
 
   return (
     <div className={Style.pageUser}>
@@ -35,19 +31,25 @@ export default function PageUser() {
         <ButtonsNavigation page={-1} text={'Назад'} />
         <ButtonsNavigation page={'/'} text={'На главную'} />
       </div>
-      <div className={Style.pageUser_userCard}>
-        <img
-          className={Style.foto}
-          src={
-            user.avatar
-              ? `http://localhost:3001/${user.avatar}`
-              : 'https://www.murrayglass.com/wp-content/uploads/2020/10/avatar-scaled.jpeg'
-          }
-          alt="аватарка"
-        />
-        <p>{`${user.name} (${user.gender}. ${user.age})`}</p>
-        <p>{user.town}</p>
-      </div>
+      {showSceleton ? (
+        <PageUserSceleton />
+      ) : err || !user ? (
+        <ErrServer textErr={errText} />
+      ) : (
+        <div className={Style.pageUser_userCard}>
+          <img
+            className={Style.foto}
+            src={
+              user.avatar
+                ? `http://localhost:3001/${user.avatar}`
+                : 'https://www.murrayglass.com/wp-content/uploads/2020/10/avatar-scaled.jpeg'
+            }
+            alt="аватарка"
+          />
+          <p>{`${user.name} (${user.gender}. ${user.age})`}</p>
+          <p>{user.town}</p>
+        </div>
+      )}
     </div>
   );
 }
