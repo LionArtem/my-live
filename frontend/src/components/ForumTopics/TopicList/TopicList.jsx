1f34c310d7d32adf27b003305878aeb71dee2bbd
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-12-15 20:08:19 +0300

    добавить подтверждение и прилоадер удаления темы

--- frontend/src/components/ForumTopics/TopicList/TopicList.jsx
+++ frontend/src/components/ForumTopics/TopicList/TopicList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Style from './TopicList.module.scss';
 
@@ -13,12 +13,22 @@ import { Link } from 'react-router-dom';
 import { getTimeLocal } from '../../../utils/utils';
 import TopicListPreloader from './TopicListPreloader';
 import EmptyPage from '../../EmptyPage/EmptyPage';
+import ModulConfirmation from '../../Moduls/ModulConfirmation/ModulConfirmation';
+import ModulePreloader from '../../Moduls/ModulePreloader/ModulePreloader';
+import {
+  selectModuleConfirmation,
+  isStatusModule,
+} from '../../../redax/slices/moduleConfirmationSlice';
 
 export default function TopicList({ getTopic, notTopics }) {
   const dispatch = useDispatch();
   const { topicsInPage, showPreloaderTopic } = useSelector(selectTopics);
   const { user } = useSelector(selectUser);
   const { token } = useSelector(selectAuth);
+  // const [moduleConfirmation, isModuleConfirmation] = useState(false);
+  const [idTopic, isIdTopic] = useState();
+  const [preloaderDelete, isPreloaderDelete] = useState(false);
+  const { statusModule } = useSelector(selectModuleConfirmation);
 
   React.useEffect(() => {
     if (token) {
@@ -26,14 +36,24 @@ export default function TopicList({ getTopic, notTopics }) {
     }
   }, []);
 
-  const deleteTopic = (evt, id) => {
-    evt.preventDefault();
-    evt.stopPropagation();
+  const deleteTopic = (id) => {
+    dispatch(isStatusModule(false));
+    isPreloaderDelete(true);
     dispatch(fetchDeleteTopic(id)).then((res) => {
       if (res.meta.requestStatus === 'fulfilled') {
         getTopic();
+      } else {
+        console.log('err');
       }
+      isPreloaderDelete(false);
     });
+  };
+
+  const openConfirmation = (evt, id) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+    isIdTopic(id);
+    dispatch(isStatusModule(true));
   };
 
   return (
@@ -60,7 +80,7 @@ export default function TopicList({ getTopic, notTopics }) {
             {user.admin && (
               <button
                 className={Style.button_delete}
-                onClick={(evt) => deleteTopic(evt, obj._id)}
+                onClick={(evt) => openConfirmation(evt, obj._id)}
               ></button>
             )}
           </Link>
@@ -68,6 +88,13 @@ export default function TopicList({ getTopic, notTopics }) {
       ) : (
         notTopics && <EmptyPage text={'Нет созданных тем.'} />
       )}
+      {statusModule && (
+        <ModulConfirmation
+          text={'Удалить тему?'}
+          confirm={() => deleteTopic(idTopic)}
+        />
+      )}
+      {preloaderDelete && <ModulePreloader text={'Удаление...'} />}
     </div>
   );
 }
