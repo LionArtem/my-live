5499eafe2debc9be2e8afd18dedc33f3a2437307
Гриневич Артем <grinevichartem@mail.ru>
2023-12-31 10:47:11 +0300

    изменить базовый адрес серввера

--- frontend/src/utils/Constants.js
+++ frontend/src/utils/Constants.js
@@ -1,4 +1,4 @@
-//const URL_SERVER = "https://api.my-live.website";
-const URL_SERVER = "http://localhost:3001";
+const URL_SERVER = "https://api.my-live.website";
+//const URL_SERVER = "http://localhost:3001";
 
 export { URL_SERVER };
